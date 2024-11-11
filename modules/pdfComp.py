f3babb322f96466f2cd1baf539a7585d73ff9f48
NishaantKrSingh <nishaantkrsingh@gmail.com>
2024-11-12 04:03:50 +0530

    configured to return data rounded up to just 2 sccurate decimal places'

--- modules/pdfComp.py
+++ modules/pdfComp.py
@@ -10,7 +10,7 @@ def compare(data1, data2):
     similarity_ratio = matcher.ratio()
 
     # Calculate the percentage difference
-    percentage_difference = (1 - similarity_ratio) * 100
+    percentage_difference = round((1 - similarity_ratio) * 100, 2)
     return percentage_difference
 
 # print(f"Percentage difference between the two strings: {percentage_difference:.2f}%")
